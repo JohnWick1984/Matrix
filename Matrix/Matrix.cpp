b366cab9de15bb78f9d538d0d51abbf4cc987f14
Евгений <@EUGENE1984>
2023-08-27 14:22:21 +0500

    Добавьте файлы проекта.

---
+++ Matrix/Matrix.cpp
@@ -0,0 +1,183 @@
+﻿// Matrix.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+template <typename T>
+class Matrix {
+private:
+    int rows;
+    int cols;
+    T** data;
+
+public:
+    Matrix(int rows, int cols) : rows(rows), cols(cols) {
+        data = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols];
+        }
+    }
+
+    ~Matrix() {
+        for (int i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    void fillFromKeyboard() {
+        cout << "Enter matrix elements:" << endl;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cin >> data[i][j];
+            }
+        }
+    }
+
+    void fillRandomly(T minVal, T maxVal) {
+        srand(time(nullptr));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = minVal + static_cast<T>(rand()) / RAND_MAX * (maxVal - minVal);
+            }
+        }
+    }
+
+    void display() const {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cout << setw(8) << data[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    Matrix operator+(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            throw runtime_error("Matrix dimensions do not match for addition.");
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator-(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            throw runtime_error("Matrix dimensions do not match for subtraction.");
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator*(const Matrix& other) const {
+        if (cols != other.rows) {
+            throw runtime_error("Matrix dimensions are not compatible for multiplication.");
+        }
+
+        Matrix result(rows, other.cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < other.cols; ++j) {
+                result.data[i][j] = 0;
+                for (int k = 0; k < cols; ++k) {
+                    result.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    Matrix operator/(const T scalar) const {
+        if (scalar == 0) {
+            throw runtime_error("Division by zero.");
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] / scalar;
+            }
+        }
+        return result;
+    }
+
+    T findMin() const {
+        T minVal = data[0][0];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] < minVal) {
+                    minVal = data[i][j];
+                }
+            }
+        }
+        return minVal;
+    }
+
+    T findMax() const {
+        T maxVal = data[0][0];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] > maxVal) {
+                    maxVal = data[i][j];
+                }
+            }
+        }
+        return maxVal;
+    }
+};
+
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    Matrix<double> matrix1(rows, cols);
+    matrix1.fillFromKeyboard();
+
+    Matrix<double> matrix2(rows, cols);
+    matrix2.fillRandomly(1.0, 10.0);
+
+    cout << "Matrix 1:" << endl;
+    matrix1.display();
+
+    cout << "Matrix 2:" << endl;
+    matrix2.display();
+
+    Matrix<double> sum = matrix1 + matrix2;
+    cout << "Sum of matrices:" << endl;
+    sum.display();
+
+    Matrix<double> difference = matrix1 - matrix2;
+    cout << "Difference of matrices:" << endl;
+    difference.display();
+
+    Matrix<double> product = matrix1 * matrix2;
+    cout << "Product of matrices:" << endl;
+    product.display();
+
+    double scalar = 2.0;
+    Matrix<double> division = matrix1 / scalar;
+    cout << "Matrix 1 divided by " << scalar << ":" << endl;
+    division.display();
+
+    cout << "Minimum element in matrix 1: " << matrix1.findMin() << endl;
+    cout << "Maximum element in matrix 2: " << matrix2.findMax() << endl;
+
+    return 0;
+}
+
